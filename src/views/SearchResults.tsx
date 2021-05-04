88022dab9420cf61a178c06d854882edc7e656b4
Conor McKeown <Conor.McKeown@terraquest.co.uk>
2021-05-04 20:59:44 +0100

    Create utils for style and fns

--- src/views/SearchResults.tsx
+++ src/views/SearchResults.tsx
@@ -1,17 +1,19 @@
 import React, { useEffect } from "react";
 import { RouteComponentProps } from "@reach/router";
 import useNasaImages from "../hooks/useNasaImages";
+import { Pager } from "../components";
 
-interface ISearchResults extends RouteComponentProps {
+interface SearchResultsProps extends RouteComponentProps {
   query?: string;
   page?: number;
 }
 
-const SearchResults: React.FC<ISearchResults> = ({ query, page }) => {
+const SearchResults: React.FC<SearchResultsProps> = ({ query, page }) => {
   const { nasaImages, loading } = useNasaImages(query!, page!);
 
   return (
     <div>
+      <Pager activePage={page!} onChange={(newPage) => console.log(newPage)} />
       <h2>This will contain search results</h2>
     </div>
   );
